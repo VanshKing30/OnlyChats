190b98aea7fff429dd502fe6065108fc7592d315
Vansh <vanshwaldeo360@gmail.com>
2025-02-04 14:50:11 +0530

    100% ready to get deployed

--- src/pages/Join.tsx
+++ src/pages/Join.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import Header from "../components/Header";
-import { GoArrowRight } from "react-icons/go";
+
 import { MdOutlineArrowCircleRight } from "react-icons/md";
 import { useRef, useState } from "react";
 
